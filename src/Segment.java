511c822152ba725ccc0f4ba8ed9abd14ce31ef00
sa2278 <160508951+sa2278@users.noreply.github.com>
2024-11-17 12:34:19 +0000

    Issues caused by point detemination, requires a full code rebuild

--- src/Segment.java
+++ src/Segment.java
@@ -1,8 +1,4 @@
-import java.awt.*;
 import java.util.*;
-import java.util.List;
-
-import javax.swing.*;
 
 public class Segment extends Point {
     double angle;
@@ -23,25 +19,47 @@ public class Segment extends Point {
         // in this case, the 200 is used to represent the current radius
         double perpendicularDistance = Math.sqrt((point.getX() - 550) * (point.getX() - 550) + (point.getY() - 550) * (point.getY() - 550));
         double chordLength = (2 * Math.sqrt(200 * 200 - perpendicularDistance * perpendicularDistance));
-        int halfChordLength = (int)(chordLength / 2);
+        double halfChordLength = (int)(chordLength / 2);
 
         //a new random angle is selected for the chord
         Random rand = new Random();
         double angle = rand.nextDouble() * 2 * Math.PI;
 
-        int highX = point.getX() + (int) (halfChordLength * Math.cos(angle));
-        int lowX = point.getX() - (int) (halfChordLength * Math.cos(angle));
+        return calculateIntercepts(this.point, angle);
 
-        int highY = point.getY() + (int) (halfChordLength * Math.sin(angle));
-        int lowY = point.getY() - (int) (halfChordLength * Math.sin(angle));
+    }
 
-        ArrayList<Point> pointPair = new ArrayList<>();
-        pointPair.add(new Point(highX, highY));
-        pointPair.add(new Point(lowX,lowY));
+    public ArrayList<Point> calculateIntercepts(Point point, Double angle){
+        double radius = 200;
+        double x = point.getX();
+        double y = point.getY();
+        double tanTheta = Math.tan(angle);
+        double a = 1 + tanTheta * tanTheta;
+        double b = -2 * (x+ tanTheta * (y - tanTheta * x));
+        double c = x * x + (y - tanTheta * x) * (y - tanTheta * x)- radius * radius;
 
-        return pointPair;
 
+        double discriminant = b * b - (4 * a * c);
 
+        // here it is assumed all lines intercept the circle at a point
+        double sqrtDiscriminant = Math.sqrt(discriminant);
+        double x1 = ((-b + sqrtDiscriminant) / (2 * a));
+        double x2 = ((-b - sqrtDiscriminant) / (2 * a));
+
+        double y1 = (int)(tanTheta * (x1 - x) + y);
+        double y2 = (int)(tanTheta * (x2 - x) + y);
+
+        ArrayList<Point> returns = new ArrayList<>();
+        returns.add(new Point(x1, y1));
+        returns.add(new Point(x2, y2));
+
+        System.out.println("x1 y1, x2 y2");
+        System.out.println(x1);
+        System.out.println(y1);
+        System.out.println(x2);
+        System.out.println(y2);
+
+        return returns;
 
     }
 
